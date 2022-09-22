95029e21262a61b0d28380157e741ffe83e880dd
Koketso Mosena <s220284059@mandela.ac.za>
2022-09-22 16:02:28 +0200

    Init Student DashBoard

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TBHAcademy.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Student()
+        {
+            return View();
+        }
+    }
+}
